6f794e67343dda4ac9ab8c05ed724caa03674bd5
YoranSchoonooghe <yoran.schoonooghe@student.howest.be>
2026-08-23 11:18:14 +0200

    Initial commit
    - Created Unreal Project
    - Added gitignore and gitattributs for LFS

---
+++ Source/BrackeysJam26/BrackeysJam26.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BrackeysJam26.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BrackeysJam26, "BrackeysJam26" );
