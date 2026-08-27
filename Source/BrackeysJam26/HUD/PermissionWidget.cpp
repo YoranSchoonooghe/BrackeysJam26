eebdf72733bd0baa94584790797a5d6cedce9498
WannesDeTollenaere <wannes.de.tollenaere@student.howest.be>
2026-08-27 14:36:39 +0200

    epische acceptatie

--- Source/BrackeysJam26/HUD/PermissionWidget.cpp
+++ Source/BrackeysJam26/HUD/PermissionWidget.cpp
@@ -2,6 +2,27 @@
 #include <Kismet/GameplayStatics.h>
 #include "BrackeysJam26/Character/PlayerCharacter.h"
 #include "BrackeysJam26/NPC/NPCCharacter.h"
+#include "Components/Button.h"
+
+void UPermissionWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+	Super::NativeTick(MyGeometry, InDeltaTime);
+
+	auto* player = Cast<APlayerCharacter>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
+	auto* NPC = player ? player->GetTargetNPC() : nullptr;
+
+	const bool bWaitingForApproval = NPC && NPC->GetState() == ENPCState::Wait;
+
+	if (AllowButton)
+	{
+		AllowButton->SetIsEnabled(bWaitingForApproval);
+	}
+
+	if (DenyButton)
+	{
+		DenyButton->SetIsEnabled(bWaitingForApproval);
+	}
+}
 
 void UPermissionWidget::AllowNPCOnBus()
 {
--- Source/BrackeysJam26/HUD/PermissionWidget.h
+++ Source/BrackeysJam26/HUD/PermissionWidget.h
@@ -4,17 +4,27 @@
 #include "Blueprint/UserWidget.h"
 #include "PermissionWidget.generated.h"
 
+class UButton;
+
 UCLASS()
 class BRACKEYSJAM26_API UPermissionWidget : public UUserWidget
 {
 	GENERATED_BODY()
-	
+
 protected:
+	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+
 	UFUNCTION(BlueprintCallable, Category = "Permission")
 	void AllowNPCOnBus();
 
 	UFUNCTION(BlueprintCallable, Category = "Permission")
 	void DenyNPCFromBus();
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidgetOptional))
+	TObjectPtr<UButton> AllowButton;
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidgetOptional))
+	TObjectPtr<UButton> DenyButton;
 
 public:
 	DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnPassengerAllowed);
